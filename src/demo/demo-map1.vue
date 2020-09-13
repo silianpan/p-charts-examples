d3b85cdd25ea00487c8fafab90da2fcfeac5e407
liupan <1206284818@qq.com>
2020-09-13 11:15:44 +0800

    add p-map component

---
+++ src/demo/demo-map1.vue
@@ -0,0 +1,21 @@
+<template>
+  <p-map :data="dataJson" :options="options" />
+</template>
+
+<script>
+import dataJson from './data/map1.json'
+export default {
+  data() {
+    return {
+      dataJson,
+      options: {
+        title: 'demo-map1-全国区域分布',
+        fieldMap: {
+          name: 'province',
+          value: 'num'
+        }
+      }
+    }
+  }
+}
+</script>
